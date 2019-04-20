d1f160da70b7b8ca3ed2145fdf0256a9e152b986
annanixon <annapnixon@gmail.com>
2019-04-19 23:36:31 -0400

    cleanup

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import PySimpleGUI as sg
-
-layout = [[sg.Text('Fuckme')],      
-          [sg.Input(), sg.FileBrowse()],      
-          [sg.OK(), sg.Cancel()]]
-
-event, (number,) = sg.Window('Get filename example').Layout(layout).Read()
-
-sg.Popup(event, number)
